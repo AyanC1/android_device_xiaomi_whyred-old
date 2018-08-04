5d7f1013d4a8dd43254c9f3f206d6b5cb8451740
MiguelNdeCarvalho <miguel@cypheros.co>
2018-08-04 21:33:47 +0000

    whyred:codex: update the CodeX Updater
    Change-Id: I3bdcab410bc6fffcdb646f775198f19aa0e450da

--- gcam/Android.mk
+++ gcam/Android.mk
@@ -3,7 +3,11 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := MGC
 LOCAL_MODULE_TAGS := optional
+<<<<<<< HEAD:gcam/Android.mk
 LOCAL_SRC_FILES := prebuild/gcam.apk
+=======
+LOCAL_SRC_FILES := APK/Codex-v1.1.apk
+>>>>>>> 363aa75... whyred:codex: update the CodeX Updater:Codex/Android.mk
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
